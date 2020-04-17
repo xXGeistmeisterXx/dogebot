415b5a50b4bc4de03a12d87d6a88576860c3aff9
xXGeistmeisterXx <tyler.geist@students.coderva.org>
2020-04-17 12:29:18 +0000

    dogebot online functionality

--- dogebot.py
+++ dogebot.py
@@ -5,191 +5,293 @@ import json
 import time
 import sys
 from keep_alive import keep_alive
+from queue import Queue
+import asyncio
+import threading
+
+os.system("clear")
 
 admins = ["355803584228622346"]
 
 game = "dogebot.help"
 
+def log(content):
+	f = open("log.txt", "a")
+	now = time.strftime('%H:%M %m/%d/%Y')
+	content = "[%s] %s\n" % (now, content)
+	f.write(content)
+	f.close()
+
 def restart():
-    os.execl(sys.executable, sys.executable, * sys.argv)
-    return("restarting")
+	os.execl(sys.executable, sys.executable, * sys.argv)
+	return("restarting")
 
 def getcommands(offline, content=None):
-    result = ""
-    if(not offline and len(content.split()) > 1 and content.split()[0] in ["dogebot.help", "dogebot.commands"]):
-        com = ""
-        for i in range(len(content.split())):
-            if i > 0:
-                com = com + content.split()[i] + " "
-        for command in commands:
-            if command["name"] == com[0:len(com) - 1]:
-                return(json.dumps(command))
-        return("`COMMAND NOT FOUND`")
-    if(offline):
-        result = "```dogebot commands (offline):\n"
-    else:
-        result = "```dogebot commands:\n"
-    for command in commands:
-        result = result + " - " + command["name"] + "\n"
-    return(result + "```")
+	result = ""
+	if(not offline and len(content.split()) > 1 and content.split()[0] in ["dogebot.help", "dogebot.commands"]):
+		com = ""
+		for i in range(len(content.split())):
+			if i > 0:
+				com = com + content.split()[i] + " "
+		for command in commands:
+			if command["name"] == com[0:len(com) - 1]:
+				return(json.dumps(command))
+		return("`COMMAND NOT FOUND`")
+	if(offline):
+		result = "```dogebot commands (offline):\n"
+	else:
+		result = "```dogebot commands:\n"
+	for command in commands:
+		result = result + " - " + command["name"] + "\n"
+	return(result + "```")
 
 def getstats():
-    result = "```dogebot stats:\n"
-    result = result + " - times restarted: " + str(stats["timesrestarted"]) + "\n"
-    result = result + " - number of messages sent: " + str(stats["messagessen"])
-    return(result + "```")
+	result = "```dogebot stats:\n"
+	result = result + " - times restarted: " + str(stats["timesrestarted"]) + "\n"
+	result = result + " - number of messages sent: " + str(stats["messagessen"])
+	return(result + "```")
 
 def addcommand(command):
-    for ocommand in commands:
-        if(ocommand["name"] == command["name"]):
-            return()
-    commands.append(command)
-    r = requests.post(url = URL + "/commands", json = json.dumps(commands))
+	for ocommand in commands:
+		if(ocommand["name"] == command["name"]):
+			return()
+	commands.append(command)
+	f = open("commands.json", "w")
+	f.write(json.dumps(commands))
+	f.close()
+	#r = requests.post(url = URL + "/commands", json = json.dumps(commands))
 
 def upcommand(command):
-    for i in range(len(commands)):
-        if(commands[i]["name"] == command["name"]):
-            commands[i] = command
-            r = requests.post(url = URL + "/commands", json = json.dumps(commands))
-            return()
+	for i in range(len(commands)):
+		if(commands[i]["name"] == command["name"]):
+			commands[i] = command
+			f = open("commands.json", "w")
+			f.write(json.dumps(commands))
+			f.close()
+			#r = requests.post(url = URL + "/commands", json = json.dumps(commands))
+			return()
 
 def delcommand(command):
-    for i in range(len(commands)):
-        if(commands[i]["name"] == command["name"]):
-            del commands[i]
-            r = requests.post(url = URL + "/commands", json = json.dumps(commands))
-    
+	for i in range(len(commands)):
+		if(commands[i]["name"] == command["name"]):
+			del commands[i]
+			f = open("commands.json", "w")
+			f.write(json.dumps(commands))
+			f.close()
+			#r = requests.post(url = URL + "/commands", json = json.dumps(commands))
+	
 def anummessent():
-    stats["messagessen"] += 1
-    requests.post(url = URL + "/stats", json = json.dumps(stats))
+	stats["messagessen"] += 1
+	f = open("stats.json", "w")
+	f.write(json.dumps(stats))
+	f.close()
+	#requests.post(url = URL + "/stats", json = json.dumps(stats))
 
 def anumres():
-    stats["timesrestarted"] += 1
-    requests.post(url = URL + "/stats", json = json.dumps(stats))
+	stats["timesrestarted"] += 1
+	f = open("stats.json", "w")
+	f.write(json.dumps(stats))
+	f.close()
+	#requests.post(url = URL + "/stats", json = json.dumps(stats))
 
 def usercommands(message, ctype):
   
-    length = len(message.content.split()[0])
-    command = message.content[length + 1:]
+	length = len(message.content.split()[0])
+	command = message.content[length + 1:]
 
-    if(ctype == "add"):
-        
-        addcommand(json.loads(command))
-        print("added " + json.loads(command)["name"])
-        
-    elif(ctype == "update"):
-        
-        upcommand(json.loads(command))
-        print("updated " + json.loads(command)["name"])
-        
-    elif(ctype == "delete"):
-        
-        delcommand(json.loads(command))
-        print("deleted " + json.loads(command)["name"])
-    
-    return("commands db updated successfully")
-    
+	if(ctype == "add"):
+		
+		addcommand(json.loads(command))
+		log("added " + json.loads(command)["name"])
+		
+	elif(ctype == "update"):
+		
+		upcommand(json.loads(command))
+		log("updated " + json.loads(command)["name"])
+		
+	elif(ctype == "delete"):
+		
+		delcommand(json.loads(command))
+		log("deleted " + json.loads(command)["name"])
+	
+	return("commands db updated successfully")
+	
+def changescore(name, num):
+  f = open("scoreboard.json", "r")
+  scoreboard = json.loads(f.readline())
+  f.close()
+  score = scoreboard[name] if name in scoreboard else 0
+  score += num
+  scoreboard[name] = score
+  f = open("scoreboard.json", "w")
+  f.write(json.dumps(scoreboard))
+  f.close()
+  return score
 
+def changescoref(content):
+  return "`changed " + content.split()[1] + " to " + str(changescore(content.split()[1], int (content.split()[2]))) + "`"
+
+def delscore(name):
+  f = open("scoreboard.json", "r")
+  scoreboard = json.loads(f.readline())
+  f.close()
+  if name in scoreboard:
+    del scoreboard[name]
+  f = open("scoreboard.json", "w")
+  f.write(json.dumps(scoreboard))
+  f.close()
+
+def delscoref(content):
+  delscore(content.split()[1])
+  return "`deleted " + content.split()[1] + "`"
+
+def getscore():
+  f = open("scoreboard.json", "r")
+  scoreboard = json.loads(f.readline())
+  f.close()
+  return scoreboard
+
+def getscoref():
+  scoreboard = getscore()
+  scoreboard = {k: v for k, v in sorted(scoreboard.items(), key=lambda item: item[1], reverse=True)}
+  result = "```SCOREBOARD:\n"
+  for name in scoreboard:
+    result = result + name + " : " + str(scoreboard[name]) + "\n"
+  if(len(scoreboard) < 1):
+    return "`EMPTY`"
+  return result + "```"
 
 client = discord.Client()
-keep_alive()
+q = Queue(maxsize = 3) 
+keep_alive(q)
 token = os.environ.get("DISCORD_BOT_SECRET")
 URL = os.environ.get("URL")
 
-#file = open("archive.json", "r")
-#obj = json.loads(file.readline())
-#commands = json.dumps(obj[0])
-#stats = json.dumps(obj[1])
-#file.close()
-#requests.post(url = URL + "/commands", json = str(commands))
-#requests.post(url = URL + "/stats", json = stats)
+file = open("commands.json", "r")
+commands = json.loads(file.readline())
+file.close()
+file = open("stats.json", "r")
+stats = json.loads(file.readline())
+file.close()
 
 borked = False
-try:
-    commands = json.loads(requests.get(url = URL + "/commands").json()["result"])
-except:
-    file = open("defaults.json", "r")
-    commands = json.loads(file.readline())[0]
-    file.close()
-    borked = True
+#try:
+#	commands = json.loads(requests.get(url = URL + "/commands").json()["result"])
+#except:
+#	file = open("defaults.json", "r")
+#	commands = json.loads(file.readline())[0]
+#	file.close()
+#	borked = True
 
-try:
-    stats = json.loads(requests.get(url = URL + "/stats").json()["result"])
-except:
-    file = open("defaults.json", "r")
-    stats = json.loads(file.readline())[1]
-    file.close()
-    borked = True
+#try:
+#	stats = json.loads(requests.get(url = URL + "/stats").json()["result"])
+#except:
+#	file = open("defaults.json", "r")
+#	stats = json.loads(file.readline())[1]
+#	file.close()
+#	borked = True
+
+async def lookformes():
+	global client
+	await client.wait_until_ready()
+	while not client.is_closed():
+		if not q.empty():
+			arr = q.get()
+			mes = ""
+			cid = 0
+			channel = None
+			if(arr[0] == "pre"):
+				mes = arr[1].lower()
+				cid = arr[2]
+				channel = client.get_channel(cid)
+			elif(arr[0] == "find"):
+				mes = arr[1].lower()
+				channel = discord.utils.get(client.get_all_channels(), guild__name=arr[2], name=arr[3].lower())
+				if channel == None:
+					log("IFTTT channel lookup failed (%s) %s" % (arr[2], arr[3].lower()))
+			if channel != None:
+				anummessent()
+				log("(%s) IFTTT sent \"%s\" in %s" % (channel.guild.name, mes, channel.name))
+				await channel.send(mes)
+		await asyncio.sleep(5)
+
+async def setgame():
+	await client.wait_until_ready()
+	while True:
+		await client.change_presence(activity=discord.Game(name=game))
+		await asyncio.sleep(60)
 
 if(not borked):
-    anumres()
+	anumres()
 
 @client.event
 async def on_message(message):
-    com = None
-    await client.change_presence(activity=discord.Game(name=game)) 
-    if(not message.author.bot):
-    
-        for command in commands:
-            notillegal = True
-            for illegal in command["illegal"]:
-                if illegal in message.content.lower():
-                    notillegal = False
-            if(notillegal and ((command["admin"] and (str(message.author.id) in admins)) or (not command["admin"]))):
-                if(command["inside"]):
-                    if(command["all"]):
-                        check = True
-                        for keyword in command["keywordz"]:
-                            if(not(keyword in message.content.lower())):
-                                check = False
-                        if(check):
-                            com = command
-                    else:
-                        for keyword in command["keywordz"]:
-                            if(keyword in message.content.lower()):
-                                com = command
-                else:
-                    for keyword in command["keywordz"]:
-                        if(keyword == message.content.lower()):
-                            com = command
+	com = None
+	if(not message.author.bot):
+		for command in commands:
+			notillegal = True
+			for illegal in command["illegal"]:
+				if illegal in message.content.lower():
+					notillegal = False
+			if(notillegal and ((command["admin"] and (str(message.author.id) in admins)) or (not command["admin"]))):
+				if(command["inside"]):
+					if(command["all"]):
+						check = True
+						for keyword in command["keywordz"]:
+							if(not(keyword in message.content.lower())):
+								check = False
+						if(check):
+							com = command
+					else:
+						for keyword in command["keywordz"]:
+							if(keyword in message.content.lower()):
+								com = command
+				else:
+					for keyword in command["keywordz"]:
+						if(keyword == message.content.lower()):
+							com = command
+		
+		if(com):
+			now = time.strftime('%H:%M %m/%d/%Y')
+			log("(%s) %s activated %s" % (message.guild.name, message.author.name, com["name"]))
+			if(not borked):
+				anummessent()
+			
+			if(com["type"] == "file"):
+				
+				myfile = discord.File("files/" + com["content"], filename=com["content"])
+				await message.channel.send(file=myfile)
+				
+			elif(com["type"] == "text"):
+				
+				await message.channel.send(com["content"])
+				
+			elif(com["type"] == "reaction"):
+				
+				for emoji in com["content"]:
+					
+					await message.add_reaction(emoji)
+				
+			elif(com["type"] == "command"):
+				
+				result = ""
+				output = {}
+				
         
-        if(com):
-            now = time.strftime('%H:%M %m/%d/%Y')
-            print("[%s] (%s) %s activated %s" % (now, message.guild.name, message.author.name, com["name"]))
-            if(not borked):
-                anummessent()
-            
-            if(com["type"] == "file"):
-                
-                myfile = discord.File("files/" + com["content"], filename=com["content"])
-                await message.channel.send(file=myfile)
-                
-            elif(com["type"] == "text"):
-                
-                await message.channel.send(com["content"])
-                
-            elif(com["type"] == "reaction"):
-                
-                for emoji in com["content"]:
-                    
-                    await message.add_reaction(emoji)
-                
-            elif(com["type"] == "command"):
-                
-                result = ""
-                output = {}
-                
-                for index in com["content"][1]:
-                    
-                    output[index] = eval(com["content"][0][index])
-                    
-                for index in com["content"][2]:
-                    if(not(index in com["content"][1])):
-                        output[index] = com["content"][0][index]
-                        
-                    result = result + str(output[index])
-                    
-                await message.channel.send(result)
 
-print("STARTED")                 
+				for index in com["content"][1]:
+					
+					output[index] = eval(com["content"][0][index])
+					
+				for index in com["content"][2]:
+					if(not(index in com["content"][1])):
+						output[index] = com["content"][0][index]
+						
+					result = result + str(output[index])
+					
+				await message.channel.send(result)
+
+log("STARTED DOGEBOT")
+client.loop.create_task(lookformes())
+client.loop.create_task(setgame())
 client.run(token)
--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,103 @@
-from flask import Flask
+from flask import Flask, request
 from threading import Thread
+import os
+import sys
+from queue import Queue
+import _thread
+import subprocess
+import time
+import json
+import requests
+
+def log(content):
+	f = open("log.txt", "a")
+	now = time.strftime('%H:%M %m/%d/%Y')
+	content = "[%s] %s\n" % (now, content)
+	f.write(content)
+	f.close()
 
 app = Flask('')
 
 @app.route('/')
 def home():
-  return "I'm alive"
+	return "I'm alive"
+
+@app.route('/ping')
+def ping():
+	log("IFTTT PING")
+	return "ping"
+
+@app.route('/status', methods=['GET', 'POST'])
+def status():
+	global status
+	if request.method == 'GET':
+		log("IFTTT GET STATUS")
+	else:
+		log("IFTTT GET STATUS (AUTO)")
+	sstatus = "ONLINE" if status else "OFFLINE"
+	url = "https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Original_Doge_meme.jpg/300px-Original_Doge_meme.jpg" if status else "https://www.meme-arsenal.com/memes/ba1e04fdbdc5fb5d724456b5efacb369.jpg"
+	obj = {"value1" : sstatus, "value2" : url}
+	purl = "https://maker.ifttt.com/trigger/status/with/key/df9JDzUJhVJ52dsn5HxnUpcwOvhRWIoPJ4kY2YC3RQT"
+	r = requests.post(purl, json = obj)
+	return "yes"
+
+@app.route('/data')
+def send():
+	global status
+	global key
+	global m
+	auth = request.args.get('token')
+	data = request.args.get('data')
+	prot = request.args.get('protocol')
+	data = data[1:]
+	prot = int(prot)
+	if key == auth:
+		if prot == 1:
+			if data == "restart":
+				log("RESTARTED BY IFTTT")
+				try:
+					m.kill()
+				except:
+					pass
+				_thread.interrupt_main()
+				os.execl(sys.executable, sys.executable, * sys.argv)
+			elif data == "stop":
+				log("STOPPED BY IFTTT")
+				status = False
+				_thread.interrupt_main()
+				try:
+					m.kill()
+				except:
+					pass
+			elif data == "start":
+				log("STARTED BY IFTTT")
+				status = True
+				m = subprocess.Popen([sys.executable, './dogebot.py'])
+				sys.exit()
+		elif prot == 400:
+			ldata = data.split()
+			q.put(["find", " ".join(ldata[2:]), ldata[0], ldata[1]])
+		elif prot == 401:
+			#fake-general, RDS
+			q.put(["pre", data, 648371226930184226])
+		elif prot == 402:
+			#dogebot-testing, RDS
+			q.put(["pre", data, 655834103048044554])
+		elif prot == 403:
+			#general, DVS
+			q.put(["pre", data, 693229884352233543])
+	return "yes"
+
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
+def keep_alive(u):
+	global key
+	global q
+	global status
+	q = u
+	t = Thread(target=run)
+	key = os.environ.get("AUTH")
+	status = True
+	t.start()
