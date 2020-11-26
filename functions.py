b6895d2d59afa3f1e5e2b33f18d156d07b9e20e9
xxgeistmeisterxx <tyler.geist1111@gmail.com>
2020-11-26 18:01:44 -0500

    take 2

--- functions.py
+++ functions.py
@@ -58,7 +58,7 @@ def delcommand(command):
 def getstats(message, conn):
 	print("got here")
 	stats = sql.getstats(conn)
-	embed = discord.Embed(title = "dogebot stats", color = discord.Color.from_hsv(40, 57, 81))
+	embed = discord.Embed(title = "dogebot stats", color = discord.Color.from_rgb(209, 170, 88))
 	embed.add_field(name = "messages sent", value = str(stats["messagessen"]), inline = True)
 	embed.add_field(name = "times restarted", value = str(stats["timesrestarted"]), inline = True)
 	embed.set_thumbnail(url = message.guild.me.avatar_url)
