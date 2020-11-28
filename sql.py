60ccb13d2fca044846e19db854d69bf99d21d624
xxgeistmeisterxx <tyler.geist1111@gmail.com>
2020-11-28 13:12:00 -0500

    fix

--- sql.py
+++ sql.py
@@ -79,22 +79,19 @@ def addcom(conn, name, content, type, inside, all, admin, keywords, illegals, lo
 	cur = conn.cursor()
 	query = "INSERT INTO commands(name,content,type,inside,'all',admin) VALUES ('{}', '{}', '{}', {}, {}, {})".format(name, content, type, int(inside), int(all), int(admin))
 	cur.execute(query)
-	conn.commit()
+	#conn.commit()
 	query = "SELECT id FROM commands WHERE name='{}' AND content='{}' AND type='{}' AND inside={} AND \"all\"={} AND admin={}".format(name, content, type, int(inside), int(all), int(admin))
-	print(query)
 	cur.execute(query)
-	id = cur.fetchall()
-	print(id)
+	id = cur.fetchall()[0]
 	for keyword in keywords:
 		query = "INSERT INTO keywords(id, keyword) VALUES ({},'{}')".format(id, keyword)
-		print(query)
 		cur.execute(query)
 	for illegal in illegals:
 		query = "INSERT INTO illegals(id, illegal) VALUES ({},'{}')".format(id, illegal)
 		cur.execute(query)
 	query = "SELECT id FROM corder"
 	cur.execute(query)
-	order = cur.fetchall()
+	order = cur.fetchall()[0]
 	print(order)
 	if loc > order[len(order) - 1]:
 		query = "INSERT INTO corder(id) VALUES ({})".format(id)
