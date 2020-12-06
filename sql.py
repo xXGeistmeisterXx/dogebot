879a477e3a423b67f0e86a10d9e627334dedc37e
xxgeistmeisterxx <tyler.geist1111@gmail.com>
2020-12-05 23:51:05 -0500

    another sillly sql

--- sql.py
+++ sql.py
@@ -94,7 +94,7 @@ def addcom(conn, name, content, type, inside, all, admin, keywords, illegals):
 
 def updatecom(conn, id, name, content, type, inside, all, admin, keywords, illegals):
 	cur = conn.cursor()
-	query = "UPDATE commands SET name = {}, content = {}, type = {}, inside = {}, 'all' = {}, admin = {} WHERE id = {}".format(name, content, type, int(inside), int(all), int(admin), int(id))
+	query = "UPDATE commands SET name = \"{}\", content = \"{}\", type = \"{}\", inside = {}, 'all' = {}, admin = {} WHERE id = {}".format(name, content, type, int(inside), int(all), int(admin), int(id))
 	cur.execute(query)
 	query = "DELETE FROM keywords WHERE id={}".format(int(id))
 	cur.execute(query)
